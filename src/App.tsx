fea43d4c6563d63cb2ebdf6cb5488a38ce787420
SitaGomes <ansitagomes@gmail.com>
2021-07-25 22:47:59 +0100

    Fix Protected Route

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import {TasksRoom} from "./Pages/TasksRoom" 
 import {HomePage} from "./Pages/HomePage"
 
-import { ProtectedRoute } from "./ProtectedRoute"
+import { ProtectedRoute } from "./Routes/ProtectedRoute"
 
 import { useAuth } from "./Hooks/useAuth"
 
 function App() {
 
-  const {isAuth, user} = useAuth()
-
-  console.log(isAuth)
+  const {isAuth} = useAuth()
   
   return (
       <Router>
